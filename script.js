ec40b5b24c0903a780ac71db594d5079308ea110
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-09-19 19:28:07 +0500

    add addBook function

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ function Book(title,author,pages,isRead){
     this.isRead = isRead;
 }
 
-function getBook(e){
+function addBook(e){
     e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
+
+    const book = new Book(title,author,pages,read);
+
+    library.push(book);
 }
